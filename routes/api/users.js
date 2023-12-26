b7aedc8230c36f48a01222cd65644bd347e70e85
Abdella Nurahmed <abd.nurahmed@gmail.com>
2023-12-26 16:10:05 +0300

    Validate request body is a valid user.

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const { body, validationResult } = require('express-validator');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-  console.log(req.body);
-  res.send('User routes');
-});
+router.post(
+  '/',
+  [
+    body('name', 'Name is required').notEmpty(),
+    body('email', 'Email is not a valid email').isEmail(),
+    body('password', 'Password should be at least 4 characters').isLength({
+      min: 4
+    })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+      return res.status(400).json({ errors: errors.array() });
+    res.send('User routes');
+  }
+);
 
 module.exports = router;
