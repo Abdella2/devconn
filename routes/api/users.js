942d5e086e8bde4bdda139b0647f5391797702c4
Abdella Nurahmed <abd.nurahmed@gmail.com>
2023-12-26 21:06:39 +0300

    Registering the user.

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const { body, validationResult } = require('express-validator');
+const User = require('../../models/User');
+const bcrypt = require('bcryptjs');
+const gravatar = require('gravatar');
 
 const router = express.Router();
 
@@ -12,11 +15,42 @@ router.post(
       min: 4
     })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty())
       return res.status(400).json({ errors: errors.array() });
-    res.send('User routes');
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (user)
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exist.' }] });
+
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+      });
+
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password, salt);
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password: hashedPassword
+      });
+
+      await user.save();
+
+      res.send('User registered');
+    } catch (err) {
+      res.status(500).send('Server error');
+    }
   }
 );
 
