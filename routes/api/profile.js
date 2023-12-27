3f955c465bf2202d432696fc30d5b21d6bd1e3fc
Abdella Nurahmed <abd.nurahmed@gmail.com>
2023-12-27 19:50:32 +0300

    Create a route to create or update a profile.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { body, validationResult } = require('express-validator');
 
 const auth = require('../../middlewares/auth');
 const Profile = require('../../models/Profile');
@@ -20,5 +21,72 @@ router.get('/me', auth, async (req, res) => {
     return res.status(500).send('Server error');
   }
 });
+
+// @desc Create or update user profile
+router.post(
+  '/',
+  [
+    auth,
+    body('status', 'Status is required').notEmpty(),
+    body('skills', 'You should have to include at least one skill').notEmpty()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+      return res.status(400).json({ errors: errors.array() });
+
+    const {
+      company,
+      website,
+      location,
+      status,
+      skills,
+      bio,
+      githubusername,
+      youtube,
+      twitter,
+      facebook,
+      linkedin,
+      instagram
+    } = req.body;
+
+    const profile = {};
+    profile.userId = req.user.id;
+    if (company) profile.company = company;
+    if (website) profile.website = website;
+    if (location) profile.location = location;
+    if (status) profile.status = status;
+    if (bio) profile.bio = bio;
+    if (githubusername) profile.githubusername = githubusername;
+    if (skills) profile.skills = skills.split(',').map((skill) => skill.trim());
+
+    profile.social = {};
+    if (youtube) profile.social = youtube;
+    if (twitter) profile.social = twitter;
+    if (facebook) profile.social = facebook;
+    if (linkedin) profile.social = linkedin;
+    if (instagram) profile.social = instagram;
+
+    try {
+      let profileInDB = await Profile.findOne({ userId: req.user.id });
+      if (profileInDB) {
+        profileInDB = await Profile.findOneAndUpdate(
+          { userId: req.user.id },
+          { $set: profile },
+          { new: true }
+        );
+        return res.json(profileInDB);
+      }
+
+      profileInDB = new Profile(profile);
+      await profileInDB.save();
+
+      return res.json(profileInDB);
+    } catch (err) {
+      console.error(err.message);
+      return res.status(500).send('Server error');
+    }
+  }
+);
 
 module.exports = router;
