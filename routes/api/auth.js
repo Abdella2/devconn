c1993a4661664deca955a9e8afc63df472ddcdae
Abdella Nurahmed <abd.nurahmed@gmail.com>
2023-12-27 12:23:09 +0300

    implement user login route.

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,5 +1,9 @@
-const express = require('express');
 const auth = require('../../middlewares/auth');
+const bcrypt = require('bcryptjs');
+const config = require('config');
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const { body, validationResult } = require('express-validator');
 const User = require('../../models/User');
 
 const router = express.Router();
@@ -13,5 +17,51 @@ router.get('/', auth, async (req, res) => {
     return res.status(500).send('Server error.');
   }
 });
+
+router.post(
+  '/',
+  [body('email').isEmail(), body('password').exists()],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+      return res
+        .status(400)
+        .json({ errors: [{ msg: 'Invalid email or password' }] });
+
+    try {
+      const { email, password } = req.body;
+
+      const user = await User.findOne({ email });
+      if (!user)
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid email or password' }] });
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch)
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid email or password' }] });
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 36000 },
+        (err, token) => {
+          if (err) throw err;
+          return res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      return res.status(500).send('Server error');
+    }
+  }
+);
 
 module.exports = router;
