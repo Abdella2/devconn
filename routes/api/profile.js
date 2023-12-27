2ab68b6fde80edf16f5a28d033e314db05141a4e
Abdella Nurahmed <abd.nurahmed@gmail.com>
2023-12-27 15:44:14 +0300

    Get current user profile.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,7 +1,24 @@
 const express = require('express');
 
+const auth = require('../../middlewares/auth');
+const Profile = require('../../models/Profile');
+
 const router = express.Router();
 
-router.get('/', (req, res) => res.send('Profile routes'));
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ userId: req.user.id }).populate(
+      'user',
+      ['name', 'avatar']
+    );
+    if (!profile)
+      return res.status(400).json({ msg: 'There is no profile for this user' });
+
+    return res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).send('Server error');
+  }
+});
 
 module.exports = router;
