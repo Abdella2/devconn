16e6ecfc35515e26078de0a04bfd37af8229a61b
Abdella Nurahmed <abd.nurahmed@gmail.com>
2023-12-28 23:14:53 +0300

    Routes to get post.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -36,4 +36,30 @@ router.post(
   }
 );
 
+// @desc Get all posts
+router.get('/', auth, async (req, res) => {
+  try {
+    const posts = await Pst.find().sort({ date: -1 });
+    return res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).send('Server error');
+  }
+});
+
+// @desc Get post by id
+router.get('/:id', auth, async (req, res) => {
+  try {
+    const post = await Pst.findById(req.params.id);
+
+    if (!post) return res.status(404).json({ msg: 'Post not found.' });
+    return res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === 'ObjectId')
+      return res.status(404).json({ msg: 'Post not found.' });
+    return res.status(500).send('Server error');
+  }
+});
+
 module.exports = router;
