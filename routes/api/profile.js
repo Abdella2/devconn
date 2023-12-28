4e69380d04dfecad7624df59d63a0c1a510a1b8c
Abdella Nurahmed <abd.nurahmed@gmail.com>
2023-12-28 11:22:07 +0300

    Route to add profile experience.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -130,4 +130,38 @@ router.delete('/', auth, async (req, res) => {
   }
 });
 
+// @desc Add profile experience
+router.put(
+  '/experience',
+  [
+    auth,
+    [
+      body('title', 'Title is required').notEmpty(),
+      body('company', 'Company is required').notEmpty(),
+      body('from', 'From date is required').notEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+      return res.status(400).json({ errors: errors.array() });
+
+    const { title, company, location, from, to, current, description } =
+      req.body;
+
+    const newExp = { title, company, location, from, to, current, description };
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+
+      profile.experience.unshift(newExp);
+      await profile.save();
+
+      return res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      return res.status(500).send('Server error');
+    }
+  }
+);
 module.exports = router;
