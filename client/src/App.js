fd19a1b4a4bf35e661cfefc2be54c47f0bc9fc76
Abdella Nurahmed <abd.nurahmed@gmail.com>
2024-01-20 14:56:30 +0300

    Create a Redux Store.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import { Fragment } from 'react';
+import { Provider } from 'react-redux';
 import { Outlet } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar';
+import store from './store';
 
 const App = () => (
-  <Fragment>
-    <Navbar />
-    <Outlet />
-  </Fragment>
+  <Provider store={store}>
+    <Fragment>
+      <Navbar />
+      <Outlet />
+    </Fragment>
+  </Provider>
 );
 
 export default App;
